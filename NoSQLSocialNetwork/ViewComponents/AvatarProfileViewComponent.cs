ba67a08badfefacb65c4b2a6b35fc371ac8072d7
DIYEPH <dinhyenphan@gmail.com>
2024-11-21 00:52:11 +0700

    Thêm Đăng ký & Header

---
+++ NoSQLSocialNetwork/ViewComponents/AvatarProfileViewComponent.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MongoDB.Bson;
+using MongoDB.Driver;
+using NoSQLSocialNetwork.Data;
+using NoSQLSocialNetwork.Entities;
+using System.Security.Claims;
+
+namespace NoSQLSocialNetwork.ViewComponents
+{
+	public class AvatarProfileViewComponent : ViewComponent
+	{
+		private readonly IMongoCollection<User>? _users;
+		public AvatarProfileViewComponent(MongoDbService mongoDbService)
+		{
+			_users = mongoDbService.Database?.GetCollection<User>("Users");
+		}
+		public IViewComponentResult Invoke()
+		{
+			if (User.Identity?.IsAuthenticated == true)
+			{
+				var claimsPrincipal = User as ClaimsPrincipal;
+				var userIdClaim = claimsPrincipal?.FindFirst(ClaimTypes.NameIdentifier);
+
+				if (userIdClaim != null)
+				{
+					var userId = ObjectId.Parse(userIdClaim.Value); // Nếu userId là ObjectId
+					var user = _users?.Find(u => u.Id == userId).FirstOrDefault();
+
+					var result = new
+					{
+						Avatar = user?.AvatarUrl,
+						Name = user?.FullName
+					};
+
+					return View(result);
+				}
+			}
+			return View(null);
+		}
+	}
+}
--- NoSQLSocialNetwork/ViewModels/SignupVM.cs
+++ NoSQLSocialNetwork/ViewModels/SignupVM.cs
@@ -2,18 +2,23 @@
 
 namespace NoSQLSocialNetwork.ViewModels
 {
-    public class LoginVM
+    public class SignupVM
     {
         [Display(Name = "Email")]
         [Required(ErrorMessage = "*")]
         [EmailAddress(ErrorMessage = "Chưa đúng định dạng email")]
         public string? Email { get; set; }
 
+        [Display(Name = "Họ và tên")]
+        [Required(ErrorMessage = "*")]
+        [MaxLength(50, ErrorMessage = "Tối đa 50 ký tự")]
+        public string? Fullname { get; set; }
+
         [Display(Name = "Mật khẩu")]
         [Required(ErrorMessage = "*")]
         [MaxLength(20, ErrorMessage = "Tối đa 20 ký tự")]
         public string? Password { get; set; }
-        [Display(Name = "Remember me?")]
-        public bool RememberMe { get; set; }
+
+
     }
 }
